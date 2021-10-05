89566c22466e0c254fa55027aba92810010084d9
DeryabinRS <deryabinrs@gmail.com>
2021-10-05 11:04:52 +0700

    add carousel block

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-AOS.init();
+window.addEventListener("DOMContentLoaded", () => {
+
+    AOS.init();
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:30,
+        autoWidth:true,
+        nav: false,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:false
+            }
+        }
+    })
+
+})
+
+
